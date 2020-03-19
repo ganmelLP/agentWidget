ef6a8164d6a5c78d81a594620711b49b43737469
Gan Melamoud <ganm@ganm-pro.lpnet.com>
2020-03-19 14:18:47 +0000

    j

--- bundle.js
+++ bundle.js
@@ -1,13 +1,20 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const rp = require('request-promise');
 
-rp('https://jsonplaceholder.typicode.com/todos/1')
-.then(function (result) {
-console.log(result +  " the result google")
-})
-.catch(function (err) {
-console.log(err +  " the result err ")
-});
+var options = {
+  uri: 'https://jsonplaceholder.typicode.com/todos/1',
+  transform: function (body) {
+      return body;
+  }
+};
+
+rp(options)
+  .then(function (result) {
+      console.log(result + " the result!")
+  })
+  .catch(function (err) {
+    console.log(err + " the err!")
+  });
 
 var SDK = lpTag.agentSDK || {};
 $(function() {
